4629461e5783d970c1e55f75d8fcef16ca06f83a
huzaifa <huzaifa776@outlook.com>
2026-04-22 18:11:43 +0100

    Add initial project structure with .gitignore, requirements, and logging setup
    - Created .gitignore to exclude environment files
    - Added requirements.txt for project dependencies
    - Implemented setup.py for package configuration
    - Established logging functionality in logger.py
    - Introduced CustomException class for error handling
    - Created __init__.py files for package initialization

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="study buddy",
+    version="0.1",
+    author="Huzaifa",
+    packages=find_packages(),
+    install_requires = requirements,
+)
